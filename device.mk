0a962250159ed9b137d2547a598238406b1dff0a
Peter Alfonso <peteralfonsojr@gmail.com>
2012-07-10 12:33:25 -0400

    \Add missing properties, relax bootloader checks
    Change-Id: Ide226c5dfd3653a8e88ff05545de45b6362dd5da
    
    Conflicts:
    
    	board-info.txt
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -20,9 +20,24 @@
 
 DEVICE_PACKAGE_OVERLAYS := device/samsung/maguro/overlay
 
+<<<<<<< HEAD
 # These are the hardware-specific features
 PRODUCT_COPY_FILES := \
 	frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml
+=======
+PRODUCT_COPY_FILES += \
+	device/samsung/toro/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+	device/sample/etc/apns-conf_verizon.xml:system/etc/apns-conf.xml \
+	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.vc_call_vol_steps=7 \
+	ro.cdma.home.operator.numeric=310004 \
+	ro.cdma.home.operator.alpha=Verizon \
+	ro.cdma.homesystem=64,65,76,77,78,79,80,81,82,83 \
+	ro.cdma.data_retry_config=default_randomization=2000,0,0,120000,180000,540000,960000 \
+	ro.cdma.otaspnumschema=SELC,1,80,99
+>>>>>>> fbbfe16... Add missing properties, relax bootloader checks
 
 $(call inherit-product, device/samsung/tuna/device.mk)
 $(call inherit-product-if-exists, vendor/samsung/maguro/device-vendor.mk)
